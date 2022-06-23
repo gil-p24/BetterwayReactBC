4257f1b0e9c52a797d1c3b9b2b80db28ad2905f2
Gilbert Polanco <gilbertpolanco@Gilbert-Polancos-MacBook-Pro.local>
2022-06-23 11:00:22 -0400

    add header

---
+++ RDBC/meme_generator/src/App.js
@@ -0,0 +1,14 @@
+//imports snippet imrc
+//imports
+import Header from "./components/Header";
+
+function App() {
+  return (
+    <div>
+      <Header/>
+      
+    </div>
+  );
+}
+
+export default App;
